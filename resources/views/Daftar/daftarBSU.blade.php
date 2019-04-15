00e4f4498b9d174eb430ef0e084a0baa3bdeae1b
Rafif Shaputra <rafifshaputra25@gmail.com>
2019-04-15 17:10:15 +0700

    Add Landing Page and template register

---
+++ resources/views/Daftar/daftarBSU.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <title>PortalSampah</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="/css/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="/css/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.min.css" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<style>
+#container-form{
+    padding-top:20px;
+}
+</style>
+</head>
+
+<body>
+    <nav  class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" style="font-size:24px;color:#03746a;font-family:'Montserrat';" href="#"><img src="img/trash.png" style="width:50px;height:50px;"/> PortalSampah</a>
+            <div style="font-size:18px;color:#03746a;font-family:'Montserrat';"class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNav">
+            </div>
+
+            </div>
+        </nav>
+    <div class="container" id="container-form">
+        <form>
+                <fieldset>
+                  <legend><b>Daftar BSU</b></legend>
+                  <div class="form-group row">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+
+                  </fieldset>
+
+
+                  <button style="border-color:#03746a;background-color:#03746a;margin-left:27%" type="submit" class="btn btn-primary">Submit</button>
+
+                </fieldset>
+              </form>
+            </div>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <title>PortalSampah</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="/css/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="/css/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.min.css" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<style>
+
+</style>
+</head>
+
+<body>
+
+  <!-- Navigation -->
+
+  <nav  class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" style="font-size:24px;color:#03746a;font-family:'Montserrat';" href="#"><img src="img/trash.png" style="width:50px;height:50px;"/> PortalSampah</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div style="font-size:18px;color:#03746a;font-family:'Montserrat';"class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#benefit">Benefit</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#partner">Partner Kami</a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#interest">Daftar</a>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">Ingin berkontribusi untuk pengelolaan sampah lebih baik?</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+
+            <div class="form-row">
+              <div class="col-12 col-md-12 mb-4 mb-md-0">
+               <a href="#interest"> <button  style="background-color:#03746a;border-color:#03746a" class="btn btn-block btn-lg btn-primary">Ayo Bergabung Bersama Kami!</button></a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Icons Grid -->
+  <section id="benefit"class="features-icons bg-light text-center">
+    <div class="container">
+    <p style="font-size:24px;">Mengapa bergabung bersama kami? </p>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div style="color:black"class="features-icons-icon d-flex">
+              <i class="icon-link m-auto " style="color:#03746a"></i>
+            </div>
+            <h3>Menghubungkan Seluruh Stakeholder</h3>
+            <p class="lead mb-0">Menghubungkan lima stakeholder, yaitu Pemerintah, BSU, BSI, Nasabah, dan Pabrik sehingga
+            seluruh data terintegrasi dan mudah diakses.
+            </p>
+          </div>
+        </div>
+        <!--<div class="col-lg-4">
+            <img style="width:180px;height:180px;border-radius:50%;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.19);;"src="img/connect.png"/>
+            <br>
+            <br>
+            <h3>Bootstrap 4 Ready</h3>
+            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+
+        </div>
+    -->
+    <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                    <i class="icon-docs m-auto " style="color:#03746a"></i>
+              </div>
+
+              <h3>Dapat dikustomisasi</h3>
+              <br>
+              <p class="lead mb-0">
+              PortalSampah dapat dikustomisasi sesuai dengan proses bisnis bank sampah mu!</p>
+            </div>
+          </div>
+        <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                    <i class="icon-graph m-auto " style="color:#03746a"></i>
+              </div>
+              <h3>Analisis Data</h3>
+              <p class="lead mb-0">
+              Menyediakan data laporan pengumpulan sampah untuk seluruh stakeholder yang dapat dikustomisasi
+              berdasarkan periode waktu tertentu.
+              </p>
+            </div>
+          </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-check m-auto " style="color:#03746a"></i>
+            </div>
+            <h3>Mudah digunakan</h3>
+            <p class="lead mb-0">Tidak terdapat perjanjian berbelit dan aplikasi didesain untuk mudah dipahami oleh orang awam teknologi.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                <div class="features-icons-icon d-flex">
+                <i class="icon-bubble m-auto" style="color:#03746a"></i>
+                </div>
+                <h3>Layanan Bantuan</h3>
+                <p class="lead mb-0">
+                Kami membuka layanan bantuan dari pukul 06.00-09.00 yang siap membantu jika terdapat pertanyaan
+                atau permasalahan
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+                <i class="icon-handbag m-auto " style="color:#03746a"></i>
+                </div>
+                <h3>Kantong Khusus</h3>
+                <p class="lead mb-0">
+                Memberikan kantong khusus pemilahan sampah untuk Nasabah untuk meningkatkan
+                engagement dan konsistensi pemilahan.
+                </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Image Showcases -->
+  <section class="showcase">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Fully Responsive Design</h2>
+          <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Updated For Bootstrap 4</h2>
+          <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Easy to Use &amp; Customize</h2>
+          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Testimonials -->
+  <section id="partner"class="testimonials text-center bg-light">
+    <div class="container">
+      <h2 class="mb-5">Our Partner. . .</h2>
+      <div class="row">
+        <div class="col-lg-4">
+            <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                <img style="color:black;"class="img-fluid rounded-circle mb-3" src="img/bsip.jpg" alt="">
+                <h5>BSI Patriot (BSIP) Kota Bekasi</h5>
+                <p class="font-weight-light mb-0">"Memiliki lebih dari 200 partner Bank Sampah Unit"</p>
+            </div>
+          </div>
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="http://banksampahrumahharum.com/wp-content/uploads/2017/06/Bank-Sampah-Induk-Rumah-Harum-23.jpg" alt="">
+            <h5>Bapak Herman - BSI Rumah Harum Depok</h5>
+            <p class="font-weight-light mb-0">"Memiliki 100 partner Bank Sampah Unit dengan nasabah mencapai 5000"</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+              <img class="img-fluid rounded-circle mb-3" src="img/klhk.png" alt="">
+              <h5>Kementrian Lingkungan Hidup</h5>
+              <p class="font-weight-light mb-0">"Kementrian yang memiliki tanggung jawab untuk mengurusi permasalahan sampah plastik di Indonesia"</p>
+            </div>
+        </div>
+
+       <!-- <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
+            <h5>Sarah W.</h5>
+            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+          </div>
+        -->
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action -->
+  <section id="interest" class="call-to-action text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h2 class="mb-4">Memiliki ketertarikan? Daftar aja dulu!</h2>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+            Saya adalah.....
+            <br>
+            <br>
+            <div class="form-row">
+              <div class="col-12 col-md-4">
+                <button style="background-color:#03746a;border-color:#03746a;" class="btn btn-block btn-lg btn-primary">Pengurus BSI</button>
+              </div>
+              <br>
+              <br>
+              <br>
+              <div class="col-12 col-md-4">
+                   <a href="/daftarBSU"> <button style="background-color:#03746a;border-color:#03746a;" class="btn btn-block btn-lg btn-primary">Pengurus BSU</button></a>
+              </div>
+              <br>
+              <br>
+              <br>
+              <div class="col-12 col-md-4">
+                <button  style="background-color:#03746a;border-color:#03746a;" class="btn btn-block btn-lg btn-primary">Calon Nasabah</button>
+              </div>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-light" style="color:#03746a;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+          <ul class="list-inline mb-2">
+            <li class="list-inline-item">
+              <a style="color:#03746a;" href="#">About</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a style="color:#03746a;" href="#">Contact</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a style="color:#03746a;"href="#">Terms of Use</a>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+            <li class="list-inline-item">
+              <a style="color:#03746a;"href="#">Privacy Policy</a>
+            </li>
+          </ul>
+          <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights Reserved.</p>
+        </div>
+        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i style="color:#03746a;" class="fab fa-facebook fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i style="color:#03746a;" class="fab fa-twitter-square fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i style="color:#03746a;"class="fab fa-instagram fa-2x fa-fw"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
