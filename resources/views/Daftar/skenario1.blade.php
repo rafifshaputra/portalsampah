cf1681f2c83fbb9498eb86d773e7f001d7ffe1b2
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-06 22:16:13 +0700

    Edit Skenario

--- resources/views/Daftar/skenario1.blade.php
+++ resources/views/Daftar/skenario1.blade.php
@@ -56,9 +56,20 @@ span{
 iframe{
     height:60%;
 }
+
+.mobile{
+    display:none
+}
 @media screen and (max-width: 1000px) {
   .column {
     width: 100%;
+  }
+  .mobile{
+      display:block;
+  }
+
+  .desktop{
+      display:none;
   }
 
   .contain{
@@ -66,122 +77,243 @@ iframe{
     padding:2px;
 }
 iframe{
-    height:30%;
+    height:50%;
+}
+
+.column3{
+    flex: 30%;
+  padding: 5px;
+    float: left;
+
+  padding: 5px;
+    width:33.33%
 }
 
 }
     </style>
 </head>
 <body style="background-color:#F3F6F5">
-<nav class="navbar navbar-default" style="background-color:#3A7765;">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#" style="color:white;font-size:20px;">TRAVELRAFIF</a>
-    </div>
+<div class="desktop">
+    <nav class="navbar navbar-default" style="background-color:#3A7765;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="#" style="color:white;font-size:20px;">TRAVELRAFIF</a>
+        </div>
 
-  </div>
-</nav>
-<div class="contain" style="padding-top:2%;background-color:white;">
+    </div>
+    </nav>
+    <div class="contain" style="padding-top:2%;background-color:white;">
+        <div class="head" style="padding-left:3%;">
+            <div style="font-size:24px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="100px;"><br></div>
+            <div style="padding-top:1%; font-size:18px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
+        </div>
+        <iframe width="100%"
+        allowfullscreen="allowfullscreen"
+            mozallowfullscreen="mozallowfullscreen"
+            msallowfullscreen="msallowfullscreen"
+            oallowfullscreen="oallowfullscreen"
+            webkitallowfullscreen="webkitallowfullscreen"
+        src="https://momento360.com/e/u/da204c0643964d41adb8ff5b9faf52a4?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=73.31666666666668"> </iframe>
+
+    <br>
+
+        <div class="rows" >
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+            </div>
+        </div>
+    <br>
+    <hr>
+
+        <div class="rows" >
+
+        <span>
+            Kamar tersedia dihari yang anda pilih tersisa sebanyak <span style="color:red">3 Kamar!</span>
+        </span>
+        <span style="padding-left:30%;">
+            <button> Pilih Kamar anda </button>
+        </span>
+        <br>
+        <br>
+        <br>
+        </div>
+
+    </div>
+    <div class="contain" style="padding-top:2%;background-color:#F3F6F5;">
+        <span style="font-size:22px;"><b>Kamar di Hotel Horison Ultima Bekasi</b> </span>
+    </div>
+    <br>
+    <div class="contain" style="background-color:white;">
+        <div class="rows" >
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:80%;"><br>
+                <div class="rows" style="width:80%" >
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                    </div>
+                </div>
+            </div>
+            <div class="column" style="padding-left:2%;padding-top:1%">
+                <p style="font-size:24px;"> Deluxe Twin Room </p>
+                <br>
+
+                <div class="rows" style="width:100%" >
+                    <div class="column" style="font-size:18px;">
+                        <b><p style="font-size:18px">Info Kamar </b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                    </div>
+
+                    <div class="column" style="font-size:18px;">
+                        <b><p style="font-size:18px">Layanan</b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="column" style="padding-left:10%;padding-top:10%">
+                <span style="color:red;">Tersisa 3 kamar lagi! </span><br>
+                <span >
+                    <button>Pesan Sekarang</button>
+                </span>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="mobile">
+    <nav class="navbar navbar-default" style="background-color:#3A7765;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="#" style="color:white;font-size:28px;">TRAVELRAFIF</a>
+        </div>
+
+    </div>
+    </nav>
+    <div class="contain" style="background-color:white;">
+        <iframe width="100%"
+        allowfullscreen="allowfullscreen"
+            mozallowfullscreen="mozallowfullscreen"
+            msallowfullscreen="msallowfullscreen"
+            oallowfullscreen="oallowfullscreen"
+            webkitallowfullscreen="webkitallowfullscreen"
+        src="https://momento360.com/e/u/da204c0643964d41adb8ff5b9faf52a4?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=73.31666666666668"> </iframe>
+
+    <br>
+
+        <div class="rows" >
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+            </div>
+        </div>
+    <br>
+
     <div class="head" style="padding-left:3%;">
-        <div style="font-size:24px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="100px;"><br></div>
-        <div style="padding-top:1%; font-size:18px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
+        <div style="font-size:36px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="100px;"><br></div>
+        <div style="padding-top:1%; font-size:26px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
     </div>
-    <iframe width="100%"
-    allowfullscreen="allowfullscreen"
-        mozallowfullscreen="mozallowfullscreen"
-        msallowfullscreen="msallowfullscreen"
-        oallowfullscreen="oallowfullscreen"
-        webkitallowfullscreen="webkitallowfullscreen"
-    src="https://momento360.com/e/u/da204c0643964d41adb8ff5b9faf52a4?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=73.31666666666668"> </iframe>
+    <hr>
+        <div class="rows" >
 
-<br>
+        <span style="padding-left:3%;font-size:20px;">
+            Kamar tersedia dihari yang anda pilih tersisa sebanyak<span style="color:red">3 Kamar!</span>
+        </span>
+        <span style="padding-left:20%;">
+            <button> Lihat Kamar </button>
+        </span>
+        <br>
+        <br>
+        <br>
+        </div>
 
-    <div class="rows" >
-        <div class="column">
-            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
-        </div>
-        <div class="column">
-            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
-        </div>
-        <div class="column">
-            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
-        </div>
-        <div class="column">
-            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
-        </div>
     </div>
-  <br>
-  <hr>
-
-    <div class="rows" >
-
-    <span>
-        Kamar tersedia dihari yang anda pilih tersisa sebanyak <span style="color:red">3 Kamar!</span>
-    </span>
-    <span style="padding-left:30%;">
-        <button> Pilih Kamar anda </button>
-    </span>
-    <br>
-    <br>
-    <br>
+    <div class="contain" style="padding-top:5%;background-color:#F3F6F5;">
+        <span style="font-size:32px;"><b>Kamar di Hotel Horison Ultima Bekasi</b> </span>
     </div>
-
-</div>
-<div class="contain" style="padding-top:2%;background-color:#F3F6F5;">
-    <span style="font-size:22px;"><b>Kamar di Hotel Horison Ultima Bekasi</b> </span>
-</div>
-<br>
-<div class="contain" style="background-color:white;">
-    <div class="rows" >
-        <div class="column">
-            <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:80%;"><br>
-            <div class="rows" style="width:80%" >
-                <div class="column">
-                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
-                </div>
-                <div class="column">
-                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
-                </div>
-                <div class="column">
-                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
-                </div>
-                <div class="column">
-                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+    <br>
+    <div class="contain" style="background-color:white;">
+        <div class="rows" >
+            <div class="column">
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:80%;"><br>
+                <div class="rows" style="width:80%" >
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                    </div>
+                    <div class="column">
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="column" style="padding-left:2%;padding-top:1%">
-            <p style="font-size:24px;"> Deluxe Twin Room </p>
-            <br>
+            <div class="column" style="padding-left:1%;padding-top:1%">
+                <p style="font-size:28px;"> Deluxe Twin Room </p>
+                <br>
 
-            <div class="rows" style="width:100%" >
-                <div class="column" style="font-size:18px;">
-                    <b><p style="font-size:18px">Info Kamar </b> </p>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                <div class="rows" style="width:100%" >
+                    <div class="column3" style="font-size:20px;">
+                        <b><p style="font-size:18px">Info Kamar </b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                    </div>
+
+                    <div class="column3" style="font-size:18px;">
+                        <b><p style="font-size:18px">Layanan</b> </p>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
+                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
+                    </div>
                 </div>
 
-                <div class="column" style="font-size:18px;">
-                    <b><p style="font-size:18px">Layanan</b> </p>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
-                    <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
-                </div>
+            </div>
+
+            <div class="column" style="padding-left:10%;padding-top:10%">
+                <span style="color:red;">Tersisa 3 kamar lagi! </span><br>
+                <span >
+                    <button>Pesan Sekarang</button>
+                </span>
             </div>
 
         </div>
-
-        <div class="column" style="padding-left:10%;padding-top:10%">
-            <span style="color:red;">Tersisa 3 kamar lagi! </span><br>
-            <span >
-                <button>Pesan Sekarang</button>
-            </span>
-        </div>
-
     </div>
 </div>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -189,4 +321,6 @@ iframe{
 <br>
 <br>
 <br>
+
+
 </html>
