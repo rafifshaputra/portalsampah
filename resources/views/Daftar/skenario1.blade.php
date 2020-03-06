c21828b32b58e5a9a8bce57b3a60b16417be1ac4
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-06 23:58:15 +0700

    edit skenario

--- resources/views/Daftar/skenario1.blade.php
+++ resources/views/Daftar/skenario1.blade.php
@@ -89,6 +89,15 @@ iframe{
     width:33.33%
 }
 
+ul.a {
+  list-style-position: outside;
+}
+button{
+    padding: 6px 15px;
+  width:50%;
+  font-size:32px;
+}
+
 }
     </style>
 </head>
@@ -241,78 +250,40 @@ iframe{
     <br>
 
     <div class="head" style="padding-left:3%;">
-        <div style="font-size:36px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="100px;"><br></div>
-        <div style="padding-top:1%; font-size:26px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
+        <div style="font-size:42px"><b> Klapa Resort</b><img style="padding-left:1%;"src="https://imgur.com/jTq0RbZ.png" width="150px;"><br></div><br>
+        <b><span style="font-size:36px"> <img src="https://imgur.com/XW23AfN.png" alt="Mountains" style="width:35px">
+            Lokasi</b></span>
+        <div style="padding-top:1%; font-size:32px;"> Jalan Uluwatu 123. Bekasi Utara. Jakarta. 17610 </div><br>
     </div>
     <hr>
-        <div class="rows" >
-
-        <span style="padding-left:3%;font-size:20px;">
-            Kamar tersedia dihari yang anda pilih tersisa sebanyak<span style="color:red">3 Kamar!</span>
-        </span>
-        <span style="padding-left:20%;">
-            <button> Lihat Kamar </button>
-        </span>
-        <br>
-        <br>
-        <br>
-        </div>
-
-    </div>
-    <div class="contain" style="padding-top:5%;background-color:#F3F6F5;">
-        <span style="font-size:32px;"><b>Kamar di Hotel Horison Ultima Bekasi</b> </span>
-    </div>
     <br>
-    <div class="contain" style="background-color:white;">
-        <div class="rows" >
-            <div class="column">
-                <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:80%;"><br>
-                <div class="rows" style="width:80%" >
-                    <div class="column">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Snow" style="width:100%;">
-                    </div>
-                    <div class="column">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Forest" style="width:100%">
-                    </div>
-                    <div class="column">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
-                    </div>
-                    <div class="column">
-                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/d8/0a/16/img-hotel.jpg" alt="Mountains" style="width:100%">
-                    </div>
-                </div>
-            </div>
-            <div class="column" style="padding-left:1%;padding-top:1%">
-                <p style="font-size:28px;"> Deluxe Twin Room </p>
-                <br>
+    <b><span style="font-size:36px">Deskripsi </span><br></b>
+      <p style="padding:15px;font-size:32px;" align="justify">  Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+        Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+        Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+        Hotel ini merupakan hotel yang terdiri dari lorem ipsum sum lorem
+      </p>
+    <hr>
+    <br>
+    <b><span style="font-size:36px">Kebijakan Hotel</span><br></b>
+    <ul class="a">
+        <li  style="padding:5px;font-size:32px;"> Check-in mulai dari jam 14.00</li>
+        <li  style="padding:5px;font-size:32px;"> Membawa identitas </li>
+        <li  style="padding:5px;font-size:32px;"> Deposit Rp100.000</li>
+    </ul>
+    <br>
+    <br>
+    <br>
 
-                <div class="rows" style="width:100%" >
-                    <div class="column3" style="font-size:20px;">
-                        <b><p style="font-size:18px">Info Kamar </b> </p>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
-                    </div>
+    <hr>
 
-                    <div class="column3" style="font-size:18px;">
-                        <b><p style="font-size:18px">Layanan</b> </p>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Max 2 Tamu <br>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> Single Bed<br>
-                        <img src="https://imgur.com/K5sza9m.png" height="20px" width="20px"> 23 sqm<br>
-                    </div>
-                </div>
 
-            </div>
 
-            <div class="column" style="padding-left:10%;padding-top:10%">
-                <span style="color:red;">Tersisa 3 kamar lagi! </span><br>
-                <span >
-                    <button>Pesan Sekarang</button>
-                </span>
-            </div>
-
-        </div>
-    </div>
+    <nav class="shadow-lg navbar fixed-bottom navbar-light rounded" style="background-color:#EFECEC;padding-bottom:50px;padding-top:50px;">
+        <a class="navbar-brand" href="#" style="font-size:30px;">Hanya tersisa <span style="color:red;">3 kamar lagi! </span><span style="padding-left:20%;">
+            <button> Lihat Kamar </button>
+        </span><br></a>
+      </nav>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -320,7 +291,9 @@ iframe{
 </body>
 <br>
 <br>
+<br><br>
 <br>
+
 
 
 </html>
