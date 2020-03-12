4a68daba23cb736621348ac6898e20146feb56f0
Rafif Shaputra <rafifshaputra25@gmail.com>
2020-03-12 08:13:51 +0700

    Edit Skenario

--- resources/views/Daftar/skenario1-rev.blade.php
+++ resources/views/Daftar/skenario1-rev.blade.php
@@ -2,238 +2,127 @@
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  <style>
 
- <!-- #region Jssor Slider Begin -->
-    <!-- Generator: Jssor Slider Composer -->
-    <!-- Source: https://www.jssor.com/rafifshaputra/image-gallery.slider/=edit -->
-    <script src="js/jssor.slider-28.0.0.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        window.jssor_1_slider_init = function() {
+body {
+    font-family: 'Poppins';
+}
+.column {
+  float: left;
+  width: 25%;
+  padding: 5px;
+}
 
-            var jssor_1_SlideshowTransitions = [
-              {$Duration:800,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:-0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:-0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:-0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,$Cols:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:0.3,$Rows:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:0.3,$Cols:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,y:-0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,$Rows:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:-0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,$Delay:20,$Clip:3,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,$Delay:20,$Clip:3,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,$Delay:20,$Clip:12,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
-              {$Duration:800,$Delay:20,$Clip:12,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
-            ];
+/* Clear floats after image containers */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
 
-            var jssor_1_options = {
-              $SlideDuration: 2000,
-              $SlideshowOptions: {
-                $Class: $JssorSlideshowRunner$,
-                $Transitions: jssor_1_SlideshowTransitions,
-                $TransitionsOrder: 1
-              },
-              $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-              },
-              $ThumbnailNavigatorOptions: {
-                $Class: $JssorThumbnailNavigator$,
-                $SpacingX: 5,
-                $SpacingY: 5
-              }
-            };
+.rows {
+  display: flex;
+}
 
-            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+.column {
+  flex: 25%;
+  padding: 5px;
+}
 
-            /*#region responsive code begin*/
 
-            var MAX_WIDTH = 980;
 
-            function ScaleSlider() {
-                var containerElement = jssor_1_slider.$Elmt.parentNode;
-                var containerWidth = containerElement.clientWidth;
+.contain{
+    margin:0px 300px;
+    padding:0px 15px;
+}
 
-                if (containerWidth) {
+button {
+  background-color: #3A7765; /* Green */
+  border: none;
+  color: white;
+  padding: 6px 15px;
+  width:200px;
 
-                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+}
 
-                    jssor_1_slider.$ScaleWidth(expectedWidth);
-                }
-                else {
-                    window.setTimeout(ScaleSlider, 30);
-                }
-            }
+span{
+    padding:10px;
+}
 
-            ScaleSlider();
+iframe{
+    height:60%;
+}
 
-            $Jssor$.$AddEvent(window, "load", ScaleSlider);
-            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-            /*#endregion responsive code end*/
-        };
-    </script>
-<style>
+.mobile{
+    display:none
+}
 
-    body {
-        font-family: 'Poppins';
-    }
-    .column {
+.mobile2{
+    display:none
+}
+
+@media screen and (max-width: 1000px) {
+  .column {
+    width: 100%;
+  }
+  .mobile{
+      display:block;
+  }
+
+  .desktop{
+      display:none;
+  }
+
+  .mobile2{
+      display:none;
+  }
+
+  .contain{
+    margin:0px 0px;
+    padding:2px;
+}
+iframe{
+    height:50%;
+}
+
+.column3{
+    flex: 30%;
+  padding: 5px;
     float: left;
-    width: 25%;
-    padding: 5px;
-    }
 
-    /* Clear floats after image containers */
-    .row::after {
-    content: "";
-    clear: both;
-    display: table;
-    }
+  padding: 5px;
+    width:33.33%
+}
 
-    .rows {
-    display: flex;
-    }
+.column4{
+    flex: 50%;
+  padding: 5px;
+    float: left;
 
-    .column {
-    flex: 25%;
-    padding: 5px;
-    }
+  padding: 5px;
+    width:60%;
+}
 
-    .contain{
-        margin:0px 300px;
-        padding:0px 15px;
-    }
 
-    button {
-    background-color: #3A7765; /* Green */
-    border: none;
-    color: white;
-    padding: 6px 15px;
-    width:200px;
+ul.a {
+  list-style-position: outside;
+}
+button{
+    padding: 15px 15px;
+  width:50%;
+  font-size:32px;
+}
+button.but{
+    padding: 15px 15px;
+  width:100%;
+  font-size:32px;
+}
 
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 18px;
-    }
-
-    span{
-        padding:10px;
-    }
-
-    iframe{
-        height:60%;
-    }
-
-    .mobile{
-        display:none
-    }
-
-    .mobile2{
-        display:none
-    }
-
-    @media screen and (max-width: 1000px) {
-        .column {
-            width: 100%;
-        }
-        .mobile{
-            display:block;
-        }
-
-        .desktop{
-            display:none;
-        }
-
-        .mobile2{
-            display:none;
-        }
-
-        .contain{
-            margin:0px 0px;
-            padding:2px;
-        }
-
-        iframe{
-            height:50%;
-        }
-
-        .column3{
-            flex: 30%;
-            padding: 5px;
-            float: left;
-            padding: 5px;
-            width:33.33%
-        }
-
-        .column4{
-            flex: 50%;
-            padding: 5px;
-            float: left;
-            padding: 5px;
-            width:60%;
-        }
-
-        ul.a {
-            list-style-position: outside;
-        }
-        button{
-            padding: 15px 15px;
-            width:50%;
-            font-size:32px;
-        }
-        button.but{
-            padding: 15px 15px;
-            width:100%;
-            font-size:32px;
-        }
-    }
-
-        /*jssor slider loading skin spin css*/
-        .jssorl-009-spin img {
-            animation-name: jssorl-009-spin;
-            animation-duration: 1.6s;
-            animation-iteration-count: infinite;
-            animation-timing-function: linear;
-        }
-
-        @keyframes jssorl-009-spin {
-            from { transform: rotate(0deg); }
-            to { transform: rotate(360deg); }
-        }
-
-        /*jssor slider arrow skin 106 css*/
-        .jssora106 {display:block;position:absolute;cursor:pointer;}
-        .jssora106 .c {fill:#fff;opacity:.3;}
-        .jssora106 .a {fill:none;stroke:#000;stroke-width:350;stroke-miterlimit:10;}
-        .jssora106:hover .c {opacity:.5;}
-        .jssora106:hover .a {opacity:.8;}
-        .jssora106.jssora106dn .c {opacity:.2;}
-        .jssora106.jssora106dn .a {opacity:1;}
-        .jssora106.jssora106ds {opacity:.3;pointer-events:none;}
-
-        /*jssor slider thumbnail skin 101 css*/
-        .jssort101 .p {position: absolute;top:0;left:0;box-sizing:border-box;background:#000;}
-        .jssort101 .p .cv {position:relative;top:0;left:0;width:100%;height:100%;border:2px solid #000;box-sizing:border-box;z-index:1;}
-        .jssort101 .a {fill:none;stroke:#fff;stroke-width:400;stroke-miterlimit:10;visibility:hidden;}
-        .jssort101 .p:hover .cv, .jssort101 .p.pdn .cv {border:none;border-color:transparent;}
-        .jssort101 .p:hover{padding:2px;}
-        .jssort101 .p:hover .cv {background-color:rgba(0,0,0,6);opacity:.35;}
-        .jssort101 .p:hover.pdn{padding:0;}
-        .jssort101 .p:hover.pdn .cv {border:2px solid #fff;background:none;opacity:.35;}
-        .jssort101 .pav .cv {border-color:#fff;opacity:.35;}
-        .jssort101 .pav .a, .jssort101 .p:hover .a {visibility:visible;}
-        .jssort101 .t {position:absolute;top:0;left:0;width:100%;height:100%;border:none;opacity:.6;}
-        .jssort101 .pav .t, .jssort101 .p:hover .t{opacity:1;}
+}
     </style>
 </head>
 <body style="background-color:#F3F6F5">
@@ -359,78 +248,30 @@
     </div>
     </nav>
     <div class="contain" style="background-color:white;">
-        <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:1180px;overflow:hidden;visibility:hidden;">
-            <!-- Loading Screen -->
-            <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
-                <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
-            </div>
-            <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:1080px;overflow:hidden;">
-                <div>
-                    <iframe data-u="image" id="myImage" width="100%"
-                    allowfullscreen="true"
-                    frameborder="0"
-                    src="https://panoraven.com/slider/kK1dNWcEZf"> </iframe>
-                    <img data-u="thumb" src="https://i.ibb.co/0hgFCQ7/gambar3.png" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/032.jpg" />
-                    <img data-u="thumb" src="img/032-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/033.jpg" />
-                    <img data-u="thumb" src="img/033-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/034.jpg" />
-                    <img data-u="thumb" src="img/034-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/035.jpg" />
-                    <img data-u="thumb" src="img/035-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/036.jpg" />
-                    <img data-u="thumb" src="img/036-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/037.jpg" />
-                    <img data-u="thumb" src="img/037-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/038.jpg" />
-                    <img data-u="thumb" src="img/038-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/039.jpg" />
-                    <img data-u="thumb" src="img/039-s190x90.jpg" />
-                </div>
-                <div>
-                    <img data-u="image" src="img/040.jpg" />
-                    <img data-u="thumb" src="img/040-s190x90.jpg" />
-                </div>
-            </div><a data-scale="0" href="https://www.jssor.com" style="display:none;position:absolute;">slideshow html</a>
-            <!-- Thumbnail Navigator -->
-            <div data-u="thumbnavigator" class="jssort101" style="position:absolute;left:0px;bottom:0px;width:980px;height:100px;background-color:#000;" data-autocenter="1" data-scale-bottom="0.75">
-                <div data-u="slides">
-                    <div data-u="prototype" class="p" style="width:190px;height:90px;">
-                        <div data-u="thumbnailtemplate" class="t"></div>
-                        <svg viewbox="0 0 16000 16000" class="cv">
-                            <circle class="a" cx="8000" cy="8000" r="3238.1"></circle>
-                            <line class="a" x1="6190.5" y1="8000" x2="9809.5" y2="8000"></line>
-                            <line class="a" x1="8000" y1="9809.5" x2="8000" y2="6190.5"></line>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-            <!-- Arrow Navigator -->
-            <div data-u="arrowleft" class="jssora106" style="width:55px;height:55px;top:162px;left:30px;" data-scale="0.75">
+        <iframe id="myImage" width="100%"
+        allowfullscreen="allowfullscreen"
+            mozallowfullscreen="mozallowfullscreen"
+            msallowfullscreen="msallowfullscreen"
+            oallowfullscreen="oallowfullscreen"
+            webkitallowfullscreen="webkitallowfullscreen"
+        src="https://momento360.com/e/u/9303891d183c47db90e91352a9b2969d?utm_campaign=embed&utm_source=other&utm_medium=other&heading=0&pitch=0&field-of-view=75"> </iframe>
 
+    <br>
+        <div class="rows" >
+            <div class="column">
+                <img id="imgcol1"onclick="imageChange()" src="https://i.ibb.co/gtKZs6z/gambar.png" alt="Snow" style="width:100%;height:180px;">
             </div>
-            <div data-u="arrowright" class="jssora106" style="width:55px;height:55px;top:162px;right:30px;" data-scale="0.75">
+            <div class="column">
+                <img id="imgcol3" onclick="imageChange3()" src="https://i.ibb.co/0hgFCQ7/gambar3.png" alt="Mountains" style="width:100%;height:180px;">
+            </div>
+            <div class="column">
+                <img id="imgcol4" onclick="imageChange4()"src="https://i.ibb.co/Pchrm5P/gambar4.png" alt="Mountains" style="width:100%;height:180px;">
+            </div>
+            <div class="column">
+                <img id="imgcol2" onclick="imageChange2()"src="https://i.ibb.co/FJcb7zV/gambar2.png" alt="Forest" style="width:100%;height:180px;">
+            </div>
 
-            </div>
         </div>
-
     <br>
 
     <div class="head" style="padding-left:3%;">
@@ -822,8 +663,6 @@ function imageChange4(){
 
 
 
-</script>
-<script type="text/javascript">jssor_1_slider_init();
 </script>
 </body>
 <br>
